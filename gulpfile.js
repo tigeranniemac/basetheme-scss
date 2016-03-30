d3fb2920afc8cb1d8a78546434db636ae49837f6
Annie McCance <amccance@atomicdust.com>
2016-03-30 15:27:11 -0500

    Added error handling so the watch task would continue after an error.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@
 
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	maps = require('gulp-sourcemaps');
-	
+	maps = require('gulp-sourcemaps'),
+	gutil = require('gulp-util'),
+	notify = require('gulp-notify');
+
+function handleErrors() {
+	gutil.beep();
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}	
 	
 gulp.task("compileSass", function(){
 	return gulp.src("scss/**/*.scss")
 		.pipe(maps.init())
 		.pipe(sass())
+		.on('error', handleErrors)
 		.pipe(maps.write('./'))
 		.pipe(gulp.dest('css'));
 });
